578353eb27c1e832356338f3ce9b9087f3db492a
igorg-rc <igor.gayvoronsky.rc@gmail.com>
2022-01-24 09:45:00 +0200

    added tranlation on detail page

--- pages/[_id].js
+++ pages/[_id].js
@@ -1,17 +1,26 @@
 import axios from "axios"
+import Image from "next/image"
 import { useRouter } from "next/dist/client/router"
 import Link from "next/link"
 
-const API_LINK = "http://193.46.199.82:5000/api"
+const API_LINK = "http://193.46.199.82:5000"
 
-export default function Contact({ contact, locale }) {
+export default function Contact({ contact }) {
   const router = useRouter()
 
   return <div className="container">
     <main>
-      <h1>Contact component</h1>
-      <h3>{ router.locale === "uk" ? contact.title_ua : contact.title_en }</h3>
-      <Link href="/">Back to index</Link>
+      <h1>{router.locale === "uk" ? <>Назва контакту</> : <>Contact title</>}</h1>
+      <Image 
+        src={`${API_LINK}/${contact.imgUrl}`}
+        width="30px"
+        height="30px"
+      />
+      <h3>{router.locale === "uk" ? contact.title_ua : contact.title_en}</h3>
+      <Link 
+        href="/">
+        {router.locale === "uk" ? "Назад на головну" : "Back to main"}
+      </Link>
       <div style={{ paddingTop: 30 }}>
         <ul style={{ listStyleType: 'none' }}>
           {router.locales.map(item => (
@@ -31,14 +40,14 @@ export default function Contact({ contact, locale }) {
 
 export const getStaticProps = async context => {
   const _id = context.params._id
-  const res = await axios.get(`${API_LINK}/contacts/${_id}`)
+  const res = await axios.get(`${API_LINK}/api/contacts/${_id}`)
   const contact = res.data.data
 
   return { props: { contact } }
 }
 
 export const getStaticPaths = async ({locales}) => {
-  const res = await axios.get(`${API_LINK}/contacts`)
+  const res = await axios.get(`${API_LINK}/api/contacts`)
   const contacts = res.data.data
 
   const paths = contacts.map(contact => (
